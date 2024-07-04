8effb5b024fcc159e50fcd5535f6910cd9369cb7
Jelena Vasić <jelena.vasic@tudublin.ie>
2024-07-04 19:44:49 +0100

    Base name fixed

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    }
-  },
+    plugins: [react()],
+    server: {
+	watch: {
+	    usePolling: true,
+	}
+    },
+    base: "/rwat_learn/",  
 });
